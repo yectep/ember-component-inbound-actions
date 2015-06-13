6e5fc2ee84f358331d96fdad087f79e1286ffabf
Chester Li <chester.li@panasonic.aero>
2015-06-12 18:34:36 -0700

    fix for prototype extensions disabled

--- addon/inbound-actions.js
+++ addon/inbound-actions.js
@@ -2,11 +2,11 @@ import Em from 'ember';
 import ActionProxy from './action-proxy';
 
 export default Em.Mixin.create({
-  _inbound_actions_setup: function() {
+  _inbound_actions_setup: Ember.on('init', function() {
     var proxy = ActionProxy.create({
       target: this
     });
 
     this.set('actionReceiver', proxy);
-  }.on('init')
+  })
 });
